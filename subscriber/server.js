e5be41c9b1cc4fc817bef6f3078bd77db8b86590
AymenKarra <76628712+AymenKarra@users.noreply.github.com>
2023-01-23 20:57:53 +0100

    create subscriber

--- subscriber/server.js
+++ subscriber/server.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
+app.use(express.json())
+app.use(cors())
+
+//reroutes requests to tempRouter.js file
+const newsRouter = require('./routes/tempRouter')
+app.use('/temp',newsRouter)
+
+//the api listens on port 3000 for incoming requests
+app.listen(3000,()=> console.log('Server started'))
